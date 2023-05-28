590edc753a266a73c72a9b54a4d31298cb94b7f2
Viníciusfsa <viniciussantos123fsa@gmail.com>
2023-05-28 16:02:24 -0300

    E2E test register

--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,4 +1,28 @@
 /* eslint-disable prettier/prettier */
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { describe, it, expect, beforeAll, afterAll } from 'vitest'
 
-test('ok', () => {})
+
+describe('register (e2e)', () => {
+
+    beforeAll(async () => {
+        await app.ready()
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it('should be able to register', async () => {
+        const response = await request(app.server)
+            .post('/users')
+            .send({
+                name: 'John Doe',
+                email: 'johndoe12141@gmail.com',
+                password: '123456'
+            })
+
+        expect(response.statusCode).toEqual(201)
+    })
+})
