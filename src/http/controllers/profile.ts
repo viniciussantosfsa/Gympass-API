da7f016e114e0df72124b7291bc49493bed6a7b0
Viníciusfsa <viniciussantos123fsa@gmail.com>
2023-05-28 10:35:24 -0300

    profile controller

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,12 +1,19 @@
 /* eslint-disable prettier/prettier */
+import { makeGetUserProfileUseCase } from '@/user-cases/factories/make-user-profile-use-case';
 import { FastifyRequest, FastifyReply } from 'fastify';
 
-
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-    await request.jwtVerify()
+    const getUserProfile = makeGetUserProfileUseCase()
 
-    console.log(request.user.sub)
+    const { user } = await getUserProfile.execute({
+        userId: request.user.sub
+    })
 
-    return reply.status(200).send()
+    return reply.status(200).send({
+        user: {
+            ...user,
+            password_hash: undefined
+        }
+    })
 }
 
