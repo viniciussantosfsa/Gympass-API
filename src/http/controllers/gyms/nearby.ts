068d46c225de4381c8a3021c25a5ff15c03d00b6
Viníciusfsa <viniciussantos123fsa@gmail.com>
2023-05-29 14:43:11 -0300

    others gyms controllers/routes

---
+++ src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,22 @@
+/* eslint-disable prettier/prettier */
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { z } from 'zod'
+import { makeFetchNearbyGymsUseCase } from '@/user-cases/factories/make-fetch-nearby-gyms-use-case'
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply): Promise<void> {
+    const nearbyGymsQuerySchema = z.object({
+        latitude: z.number().refine(value => { return Math.abs(value) <= 90 }),
+        longitude: z.number().refine((value) => { return Math.abs(value) <= 180 })
+    })
+
+    const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.body)
+
+    const fetchNearbyUseCase = makeFetchNearbyGymsUseCase()
+
+    const { gyms } = await fetchNearbyUseCase.execute({
+        userLatitude: latitude,
+        userLongitude: longitude
+    })
+
+    return reply.status(201).send({ gyms })
+}
